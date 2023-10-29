1073da7bfdca95fa754f8d321f4089fa43833f7b
jfndi <143571177+jfndi@users.noreply.github.com>
2023-10-29 11:10:39 +0100

    Add comments to the bridge project.

--- Structural/bridge/bridge.cpp
+++ Structural/bridge/bridge.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+/*
+ * Bridge is a structural design pattern that divides business logic or huge class
+ * into separate class hierarchies that can be developed independently. One of 
+ * these hierarchies (often called the Abstraction) will get a reference to an 
+ * object of the second hierarchy (Implementation).
+ */
+
 using namespace std;
 
 class Implementor
